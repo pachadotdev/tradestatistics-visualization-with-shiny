02f6423ed54671fc169af23a6b88e2a84b613e75
Pachamaltese <mvargas@dcc.uchile.cl>
2019-12-05 15:22:29 -0300

    working embed-trade

--- embed-trade/R/app_server.R
+++ embed-trade/R/app_server.R
@@ -2,13 +2,77 @@
 #' @import shiny
 #' @import shinyjs
 #' @importFrom magrittr %>%
-#' @importFrom dplyr filter select rename
-#' @importFrom stringr str_sub
+#' @importFrom dplyr filter arrange select rename tibble
+#' @importFrom jsonlite fromJSON
+#' @importFrom purrr as_vector
+#' @importFrom stringr str_sub str_length
 #' @importFrom glue glue
 #' @importFrom tidyr gather
 #' @importFrom tradestatistics ots_create_tidy_data
-#' @importFrom highcharter hchart hcaes hc_colors hc_title hc_exporting
+#' @importFrom highcharter hchart hcaes hc_colors hc_title hc_exporting renderHighchart JS
 app_server <- function(input, output, session) {
+  # URLs --------------------------------------------------------------------
+  
+  # base_url <- "http://localhost:8080"
+  base_url <- "https://api.tradestatistics.io"
+  site_url <- "shiny.tradestatistics.io"
+  
+  # Tables ------------------------------------------------------------------
+  
+  countries <- tradestatistics::ots_countries %>%
+    select(country_iso, country_name_english)
+  
+  products <- tradestatistics::ots_products %>%
+    dplyr::filter(stringr::str_length(product_code) %in% c(2, 4)) %>%
+    dplyr::arrange(product_code)
+  
+  communities <- tradestatistics::ots_communities
+  
+  # Choices -----------------------------------------------------------------
+  
+  # choices trick by Andrea Gao
+  # http://gytcrt.github.io/gytcrt.github.io/2016/08/11/RShiny-easily-passing-a-long-list-of-items-to-selectInput-choices/
+  
+  available_tables <- as.list(c("select", "yr", "yrp"))
+  names(available_tables) <- c("Select", "Multilateral trade", "Bilateral trade")
+  
+  available_years <- sprintf("%s/year_range", base_url) %>%
+    jsonlite::fromJSON() %>%
+    purrr::as_vector()
+  
+  available_years_min <- min(available_years)
+  available_years_max <- max(available_years)
+  
+  available_reporters_iso <- as.list(countries$country_iso)
+  available_reporters_iso <- c("all", available_reporters_iso[grep("^c-|all", available_reporters_iso, invert = T)])
+  names(available_reporters_iso) <- c("the World", as.vector(countries$country_name_english[grep("^Alias", countries$country_name_english, invert = T)]))
+  
+  reporters_to_display <- dplyr::tibble(
+    available_reporters_iso = purrr::as_vector(available_reporters_iso),
+    available_reporters_names = names(available_reporters_iso)
+  )
+  
+  # Bookmarking -------------------------------------------------------------
+  
+  enableBookmarking(store = "url")
+  
+  # Highcharts --------------------------------------------------------------
+  
+  hc_export_menu <- list(
+    list(text="Download PNG image",
+         onclick=JS("function () { 
+                  this.exportChart({ type: 'image/png' }); }")),
+    list(text="Download JPEG image",
+         onclick=JS("function () { 
+                  this.exportChart({ type: 'image/jpeg' }); }")),
+    list(text="Download SVG vector image",
+         onclick=JS("function () { 
+                  this.exportChart({ type: 'image/svg+xml' }); }")),
+    list(text="Download PDF document",
+         onclick=JS("function () { 
+                  this.exportChart({ type: 'application/pdf' }); }"))
+  )
+  
   # Input -------------------------------------------------------------------
   
   y1 <- reactive({
@@ -118,7 +182,7 @@ app_server <- function(input, output, session) {
   
   # Output ------------------------------------------------------------------
   
-  output$trade_bars_aggregated <- renderHighchart({
+  output$trade_bars_aggregated <- highcharter::renderHighchart({
     trade_bars_aggregated()
   })
   
--- embed-trade/www/js/copy-url.js
+++
@@ -1,5 +0,0 @@
-function copyUrl() {
-  var copyText = document.getElementById("Url");
-  copyText.select();
-  document.execCommand("copy");
-} 
