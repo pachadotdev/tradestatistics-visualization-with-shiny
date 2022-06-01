e7b7f468de05917fa7f020e8c682a2b132191e95
Mauricio Vargas <mavargas11@uc.cl>
2022-06-01 01:39:01 -0400

    define counterfactual

--- 99_draft_simulation.R
+++ 99_draft_simulation.R
@@ -10,59 +10,56 @@ df_dtl_sim <- reactive({
   
   ### 3.1. apply filters ----
   
-  tbl_sql <- if (any(inp_sim_product_filter() != "all")) {
-    "yrpc"
-  } else {
-    "yrp"
-  }
+  d <- tbl(con, "yrp") %>%
+    filter(year %in% !!inp_sim_y())
   
-  d <- tbl(con, tbl_sql)
+  ### 3.2. aggregate and transform data ----
   
   d <- d %>%
-    filter(
-      year %in% !!inp_sim_y()
+    select(year, importer = reporter_iso, exporter = partner_iso, 
+           trade = trade_value_usd_imp)
+  
+  # add GRAVITY variables
+  
+  d <- d %>%
+    mutate(
+      country1 = pmin(importer, exporter, na.rm = T),
+      country2 = pmax(importer, exporter, na.rm = T)
+    ) %>%
+    inner_join(
+      tbl(con, "distances") %>% 
+        select(country1, country2, dist, contig, comlang_off, colony),
+      by = c("country1", "country2")
     )
   
-  wt_sim$inc(1)
-  
-  if (any(inp_sim_product_filter() %in% "vaccine")) {
-    d <- d %>% 
-      left_join(
-        tbl(con, "vaccine_inputs")
-      ) %>% 
-      mutate(
-        section_code = case_when(
-          is_vaccine_input == 1L ~ "vaccine",
-          TRUE ~ section_code
-        )
-      )
-  }
-  
-  if (any(inp_sim_product_filter() != "all")) {
-    d <- d %>%
-      filter(section_code %in% !!inp_sim_product_filter())
-  }
-  
-  wt_sim$inc(1)
-  
-  #### aggregate and transform data ----
+  # add RTA data
   
   d <- d %>%
-    select(year, importer = reporter_iso, exporter = partner_iso, trade = trade_value_usd_imp) %>%
-    group_by(year, importer, exporter) %>%
-    summarise(trade = sum(trade, na.rm = T)) %>%
-    ungroup()
+    left_join(
+      tbl(con, "rtas") %>%
+        filter(year %in% !!inp_sim_y()),
+      by = c("year", "country1", "country2")
+    ) %>%
+    mutate(
+      rta = case_when(
+        is.na(rta) ~ 0L,
+        TRUE ~ rta
+      )
+    ) %>%
+    select(-c(country1,country2))
+  
+  # transform factors
   
   d <- d %>% 
     mutate(
       intl = ifelse(importer != exporter, 1, 0),
       importer = case_when(
-        importer == imp_sim_reference() ~ paste0("0-", imp_sim_reference())
-        TRUE ~ imp_sim_reference()
+        importer == !!imp_sim_reference() ~ paste0("0_", !!imp_sim_reference()),
+        TRUE ~ importer
       ),
       exporter = case_when(
-        exporter == imp_sim_reference() ~ paste0("0-", imp_sim_reference())
-        TRUE ~ imp_sim_reference()
+        exporter == !!imp_sim_reference() ~ paste0("0_", !!imp_sim_reference()),
+        TRUE ~ exporter
       )
     )
   
@@ -79,7 +76,7 @@ df_dtl_sim <- reactive({
     group_by(year) %>%
     mutate(e_r = max(
       case_when(
-        importer == paste0("0-", imp_sim_reference()) ~ e,
+        importer == paste0("0_", !!imp_sim_reference()) ~ e,
         TRUE ~ NA_real_
       ), 
       na.rm = T
@@ -88,209 +85,131 @@ df_dtl_sim <- reactive({
   d <- d %>% 
     # Pairing variable for the internal dyads for the fixed effects
     mutate(
-      exp_year = paste0(exporter, year),
-      imp_year = paste0(importer, year),
-      pair_id = paste(importer, exporter, sep = "_"),
-      pair_id_2 = case_when(
-        exporter == importer ~ "0-intra",
-        TRUE ~ pair_id
+      exp_year = paste(exporter, year, sep = "_"),
+      imp_year = paste(importer, year, sep = "_"),
+      importer_exporter = paste(importer, exporter, sep = "_"),
+      importer_exporter2 = case_when(
+        exporter == importer ~ "0_intra",
+        TRUE ~ importer_exporter
       )
-    ) %>% 
+    )
     
+  d <- d %>% 
     # To filter the cases where the sum by dyad is zero
-    group_by(pair_id) %>%
-    mutate(sum_trade = sum(trade))
+    group_by(importer_exporter) %>%
+    mutate(sum_trade = sum(trade, na.rm = T)) %>% 
+    ungroup()
   
-  #### collect data ----
+  ### 3.3 collect data ----
   
   d <- d %>% 
     collect() %>% 
     arrange(year, importer, exporter)
-  
-  wt_sim$inc(1)
-  
-  ### 3.2. add geo dist data ----
-  
-  d <- d %>%
-    mutate(
-      country1 = pmin(reporter_iso, partner_iso),
-      country2 = pmax(reporter_iso, partner_iso)
-    ) %>%
-    inner_join(
-      tbl(con, "distances") %>% 
-        select(country1, country2, dist, contig, comlang_off, colony) %>% 
-        collect(),
-      by = c("country1", "country2")
-    )
-  
-  wt_sim$inc(1)
-  
-  ### 3.3. add RTA data ----
-  
-  d <- d %>%
-    left_join(
-      tbl(con, "rtas") %>%
-        filter(year %in% !!inp_sim_y()) %>%
-        collect(),
-      by = c("year", "country1", "country2")
-    ) %>%
-    mutate(
-      rta = case_when(
-        is.na(rta) ~ 0L,
-        TRUE ~ rta
-      )
-    ) %>%
-    select(-c(country1,country2))
-  
-  wt_sim$inc(2)
 
+  wt_sim$inc(1)
+  
   ### 3.4. convert dollars in time ----
   
   if (inp_sim_convert_dollars() != "No conversion") {
     d <- gdp_deflator_adjustment(d, as.integer(inp_sim_convert_dollars()))
   }
-  
-  # OK HASTA ACA
-  
-  ### 3.7. add MFN data ----
-  
-  if (any(raw_rhs_sim() %in% "mfn")) {
-    tar <- tbl(con, "tariffs") %>%
-      filter(
-        year %in% !!inp_sim_y()
-      )
-    
-    trd <- tbl(con, "yrpc") %>% 
-      filter(
-        year %in% !!inp_sim_y() & reporter_iso %in% !!inp_sim_riso()
-      )
-    
-    if (any(inp_sim_piso() != "all")) {
-      tar <- tar %>%
-        filter(
-          # here we need the applied tariffs when the product gets to destination
-          reporter_iso %in% !!inp_sim_piso()
-        )
-      
-      trd <- tbl(con, "yrpc") %>% 
-        filter(
-          partner_iso %in% !!inp_sim_riso()
-        )
-    }
-    
-    if (any(inp_sim_product_filter() %in% "vaccine")) {
-      trd <- trd %>% 
-        left_join(
-          tbl(con, "vaccine_inputs")
-        ) %>% 
-        mutate(
-          section_code = case_when(
-            is_vaccine_input == 1L ~ "vaccine",
-            TRUE ~ section_code
-          )
-        )
-    }
-    
-    if (length(inp_sim_product_filter()) > 0) {
-      trd <- trd %>%
-        filter(section_code %in% !!inp_sim_product_filter())
-    }
-    
-    trd <- trd %>%
-      select(year, reporter_iso, partner_iso, commodity_code, trade_value_usd_exp) %>% 
-      inner_join(
-        tar %>% 
-          select(year, partner_iso = reporter_iso, commodity_code, mfn = simple_average),
-        by = c("year", "partner_iso", "commodity_code")
-      )
-    
-    trd <- trd %>%
-      select(year, reporter_iso, partner_iso, trade_value_usd_exp, mfn) %>%
-      collect() %>%
-      group_by(year, reporter_iso, partner_iso) %>%
-      summarise(
-        mfn = weighted.mean(mfn, trade_value_usd_exp, na.rm = T)
-      ) %>%
-      ungroup()
-    
-    rm(tar)
-    
-    d <- d %>% inner_join(trd); rm(trade)
-  }
-  
+
   wt_sim$inc(1)
   
   gc()
   
-  print(c(raw_lhs_sim(), raw_rhs_sim()))
-  
-  return(
-    # this is not elegant, but works well with polynomials, logs, etc in formulas
-    d[,
-      colnames(d) %in% 
-        c("year", "reporter_iso", "partner_iso",
-          lhs_sim(), rhs_sim(), raw_lhs_sim(), raw_rhs_sim(),
-          "remoteness_exp", "remoteness_imp", "cluster_pairs"
-        )
-    ]
-  )
+  return(d)
 }) %>% 
   bindCache(
-    inp_sim_y(), inp_sim_riso(), inp_sim_piso(), inp_sim_type(), inp_sim_zero(),
-    inp_sim_convert_dollars(), inp_sim_cluster(), inp_sim_product_filter(),
-    fml_sim(), lhs_sim(), rhs_sim(), raw_lhs_sim(), raw_rhs_sim()
+    inp_sim_y(), imp_sim_reference(), inp_sim_convert_dollars()
   ) %>%
   bindEvent(input$sim_go)
 
-df_dtl_2_sim <- eventReactive(input$sim_go, {
-  ### 3.8. join with custom data ----
-  
-  if (nrow(sim_custom_data()) > 0) {
-    d <- df_dtl_sim() %>% inner_join(sim_custom_data())
-  } else {
-    d <- df_dtl_sim()
-  }
-  
-  return(d)
-})
-
-## 4. Fit model ----
+## 2. Fit model ----
 
 fit_sim <- eventReactive(input$sim_go, {
   print("Fitting model...")
   
-  fml <- as.formula(fml_sim())
+  ### Step 1: Solve the baseline gravity model ----
   
-  if (any(inp_sim_type() %in% c("ols", "olsrem", "olsfe"))) {
-    if (inp_sim_cluster() == "yes") {
-      m <- tryCatch(
-        feols(fml, df_dtl_2_sim(), cluster = ~cluster_pairs),
-        error = function(e) { custom_regression_error() }
-      )
-    } else {
-      m <- tryCatch(
-        feols(fml, df_dtl_2_sim()),
-        error = function(e) { custom_regression_error() }
-      )
-    }
-  }
+  d <- df_dtl_sim()
   
-  if (inp_sim_type() == "ppml") {
-    if (inp_sim_cluster() == "yes") {
-      m <- tryCatch(
-        feglm(fml, df_dtl_2_sim(), cluster = ~cluster_pairs, family = quasipoisson(link = "log")),
-        error = function(e) { custom_regression_error() }
-      )
-    } else {
-      m <- tryCatch(
-        feglm(fml, df_dtl_2_sim(), family = quasipoisson(link = "log")),
-        error = function(e) { custom_regression_error() }
-      )
-    }
-  }
+  fit_baseline <- fepois(
+    trade ~ rta | exp_year + imp_year + importer_exporter2,
+    data = filter(d, sum_trade > 0),
+    glm.iter = 500
+  )
   
-  wt_sim$inc(2)
-  gc()
+  d <- d %>%
+    mutate(
+      fe_exporter_bln = fixef(fit_baseline)$exp_year[exp_year],
+      fe_importer_bln = fixef(fit_baseline)$imp_year[imp_year],
+      fe_pair_id_2_bln = fixef(fit_baseline)$importer_exporter2[importer_exporter2]
+    )
+  
+  d <- d %>%
+    mutate(
+      tij_bar = exp(fe_pair_id_2_bln),
+      tij_bln = exp(fe_pair_id_2_bln + fit_baseline$coefficients["rta"] * rta)
+    )
+  
+  d_slice <- d %>%
+    filter(year == !!imp_sim_y2(), exporter != importer)
+  
+  fit_costs <- fepois(
+    tij_bar ~ log(dist) + contig + comlang_off + colony | exporter + importer,
+    data = d_slice,
+    glm.iter = 500
+  )
+  
+  d_slice <- d_slice %>%
+    mutate(tij_no_rta = predict(fit_costs, d_slice)) %>%
+    select(exporter, importer, tij_no_rta)
+  
+  d <- d %>%
+    filter(year == !!imp_sim_y2()) %>%
+    left_join(d_slice, by = c("importer", "exporter")) %>%
+    mutate(
+      tij_bar = ifelse(is.na(tij_bar), tij_no_rta, tij_bar),
+      tij_bln = ifelse(is.na(tij_bln), tij_bar * exp(fit_baseline$coefficients["rta"] * rta), tij_bln)
+    ) %>%
+    select(-tij_no_rta)
+  
+  fit_constrained <- fepois(
+    trade ~ 0 | exporter + importer,
+    data = d,
+    offset = ~log(tij_bln),
+    glm.iter = 500
+  )
+  
+  d <- d %>%
+    mutate(tradehat_bln = predict(fit_constrained, d)) %>%
+    group_by(exporter) %>%
+    mutate(xi_bln = sum(tradehat_bln * (exporter != importer))) %>%
+    ungroup()
+  
+  d <- d %>%
+    mutate(
+      fe_exporter_cns = fixef(fit_constrained)$exporter[exporter],
+      fe_importer_cns = fixef(fit_constrained)$importer[importer]
+    )
+  
+  d <- d %>%
+    mutate(
+      omr_bln = y * e_r/ exp(fe_exporter_cns),
+      imr_bln = e / (exp(fe_importer_cns) * e_r)
+    )
+  
+  ### Step II: Define a counterfactual scenario ----
+  
+  region <- !!imp_sim_countries()
+  
+  d <- d %>%
+    mutate(
+      rta_no_region = ifelse(exporter %in% region & importer %in% region, 0, rta),
+      tij_cfl = tij_bar * exp(fit_baseline$coefficients["rta"] * rta_no_region)
+    )
+  
   
   wt_sim$close() 
   return(m)
